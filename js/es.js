c0cb4deee58bb0438384ff630c2e573c1513427f
Herve <herve.herbrich@gmail.com>
2018-04-27 17:37:13 +0200

    Test

---
+++ js/es.js
@@ -0,0 +1,326 @@
+Snipcart.execute('registerLocale', 'es', {
+yes:
+"Sí",
+no:
+"No",
+print:
+"Imprimir",
+download_as_pdf:
+"Descargar como PDF",
+checkout:
+"Realizar pedido",
+close:
+"Cerrar",
+name:
+"Nombre y Apellidos",
+company_name:
+"Nombre de la Compañia (Opcional)",
+share_by_email:
+"Compartir por email",
+email:
+"Email",
+password:
+"Contraseña",
+confirm_password:
+"Confirmar contraseña",
+ok:
+"OK",
+send:
+"Enviar",
+address_1:
+"Dirección",
+address_2:
+"Dirección 2",
+city:
+"Ciudad",
+postal_code:
+"Código postal",
+phone:
+"Teléfono",
+previous:
+"Paso anterior",
+next:
+"Siguiente paso",
+finalize:
+"Finalizar",
+country:
+"País",
+subtotal:
+"Subtotal",
+rebate:
+"Descuento",
+apply_promo_code:
+"Aplicar código de promoción",
+my_cart:
+"Carrito",
+my_cart_content:
+"Contenido del carrito",
+shipping_method:
+"Método de envío",
+payment_method:
+"Forma de pago",
+confirm_order:
+"Confirmar pedido",
+bill_me_later:
+"Facturar más tarde",
+bill_me_later_explanation:
+"La factura se enviará por correo electrónico.",
+promo_code_applied_successfully:
+"Descuento aplicado con éxito.",
+promo_code_is_invalid:
+"El código promocional es inválido.",
+promo_code_is_expired:
+"El código promocional ha caducado.",
+promo_code_code:
+"¿Tiene código promocional?",
+promo_code_rate_on_order:
+"descuento en el pedido",
+promo_code_alternate_price:
+"precio especial en productos",
+total:
+"Total",
+province_state:
+"Estado / Provincia",
+billing_address:
+"Dirección de facturación",
+shipping_address:
+"Dirección de envío",
+payment_informations:
+"Información de pago",
+payment_informations_bill_me_later:
+"Pagaré más tarde",
+payment_informations_paypalexpress:
+"Pagar con Paypal",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"Enero",
+months_february:
+"Febrero",
+months_march:
+"Marzo",
+months_april:
+"Abril",
+months_may:
+"Mayo",
+months_june:
+"Junio",
+months_july:
+"Julio",
+months_august:
+"Agosto",
+months_september:
+"Septiembre",
+months_october:
+"Octubre",
+months_november:
+"Noviembre",
+months_december:
+"Diciembre",
+cart_items_table_item:
+"Artículo",
+cart_items_table_description:
+"Descripción",
+cart_items_table_quantity:
+"Cantidad",
+cart_items_table_unit_price:
+"Precio unitario",
+cart_items_table_total_price:
+"Precio total",
+cart_empty_text:
+"El carrito está vacío. Seleccione algún producto antes de pasar por caja.",
+new_account_form_create_new_account:
+"Crear una cuenta",
+new_account_form_create_new_account_action:
+"Crear una cuenta",
+login_form_having_an_account:
+"Iniciar sesión",
+login_form_login_action:
+"Iniciar sesión",
+login_form_forgot_password_action:
+"He olvidado mi contraseña",
+forgot_password_forgot_your_password:
+"¿Olvidó su contraseña?",
+forgot_password_please_enter_email:
+"Indíquenos su email y le enviaremos un correo electrónico con un enlace para restablecer su contraseña.",
+forgot_password_success_email_sent:
+"Correo electrónico enviado",
+forgot_password_email_sent_message:
+"Le hemos enviado un correo electrónico con las instrucciones para  restablecer la contraseña. Consulte el correo y siga los pasos.",
+login_checkout_as_guest:
+"Pagar como invitado",
+login_checkout_as_guest_notice:
+"Al final del proceso se le ofrecerá la posibilidad de crear una cuenta utilizando la información que introdujo durante el proceso de pago.",
+shipping_address_same_as_billing:
+"Usar esta dirección para el envío",
+shipping_method_method_name:
+"Método de envío",
+shipping_method_shipping_price:
+"Precio del envío",
+shipping_method_failure_message:
+"No hemos sido capaces de encontrar un método de envío disponible. Por favor, asegúrese de que su dirección de envío es correcta y vuelva a intentarlo.",
+shipping_method_failure_click_here_to_edit:
+"Haga clic aquí para editar la dirección del envío",
+payment_method_card_holder:
+"Nombre en la tarjeta",
+payment_method_card_type:
+"Tipo de tarjeta",
+payment_method_card_number:
+"Número de tarjeta",
+payment_method_card_cvc:
+"CVC",
+payment_method_card_exp_month:
+"Mes de vencimiento",
+payment_method_card_exp_year:
+"Año de vencimiento",
+payment_method_cvc_infos:
+"El CVC es el número de seguridad de 3 dígitos detrás de su tarjeta de crédito.",
+create_an_account:
+"Crear una cuenta",
+why_create_account:
+"Para una comprobación más rápida en su próximo pedido, simplemente introduzca la contraseña e inicie sesión.",
+reset_password:
+"Restablecer contraseña",
+reset_password_success:
+"Contraseña restablecida con éxito",
+reset_password_changed:
+"Su contraseña ha sido cambiada.",
+reset_password_click_here_to_login:
+"Clic aquí para iniciar sesión",
+thankyou_message:
+"Muchas gracias por su compra. La factura ha sido enviada por correo electrónico. ",
+thankyou_submessage:
+"Pronto recibirá un email de confirmación.",
+account_created_successfully:
+"Cuenta creada con éxito",
+account_created_successfully_message:
+"Su cuenta ha sido creada correctamente, gracias.",
+errors_required:
+"Campo obligatorio",
+errors_email_must_be_unique:
+"Ya existe un usuario con este correo electrónico",
+errors_both_password_must_match:
+"Ambas contraseñas deben coincidir.",
+errors_email_must_be_valid:
+"El email debe ser válido",
+errors_email_does_not_match_any_existing_user:
+"No existe ningún usuario con este correo electrónico",
+errors_email_does_not_match_reset_password_request:
+"La dirección de email no coincide con ninguna solicitud de cambio de contraseña.",
+errors_reset_password_token_expired:
+"El tiempo para restablecer la contraseña ha caducado.",
+errors_invalid_authentication_infos:
+"Información de autenticación no válida",
+error_payment_items_empty:
+"Parece que su pedido es incorrecto, por favor, vuelva a cargar la página. No hemos realizado ningún cargo.",
+error_payment_items_are_invalid:
+"No hemos podido completar su pedido. Parece que uno de los artículos en su carrito tiene un precio incorrecto.",
+error_crawling_failed:
+"No hemos sido capaces de validar su pedido y no hemos realizado ningún cargo. Por favor, inténtelo de nuevo en unos minutos.",
+powered_by:
+"Powered and secured by",
+promocode_rate_format:
+"{0}% de descuento en su pedido",
+promocode_amount_format:
+"{0} de descuento en su pedido",
+shipping_method_business_days:
+"{0} días laborales",
+shipping_method_business_day:
+"{0} día laboral",
+shipping_method_delivery_time:
+"Para el {0}", // By 2013-11-28
+welcome:
+"Bienvenido",
+back:
+"Atrás",
+order_infos:
+"Informacion del pedido",
+generic_error_title:
+"Oops, hubo un error.",
+promocode_deleted_at_checkout:
+"El descuento que utiliza ha alcanzado su límite de uso mientras estaba haciendo su pedido. Disculpe las molestias.",
+continue_shopping:
+"Continuar comprando",
+payment_required_message:
+"El carrito ha sido desactivada para esta web. Si es el administrador, por favor acceda a Snipcart y resuelva el problema.",
+payment_require_title:
+"El carrito está desactivado.",
+configuration_problem:
+"Problema en la configuración.",
+additionnal_information:
+"Introduzca un mensaje abajo si desea enviar comentarios o más información sobre este problema.",
+send_error:
+"Enviar este error al propietario del sitio web.",
+message_sent:
+"Mensaje enviado, gracias",
+paypalexpress_loading:
+"Será redirigido a Paypal para hacer el pago.",
+paypalexpress_cancelled:
+"Transacción cancelada. Puede hacer clic en el botón de abajo para intentarlo de nuevo o volver a la tienda.",
+retry:
+"Vuelva a intentarlo",
+error_crawlingfailed_title:
+"Algo salió mal en la validación de su pedido, no se preocupe, no se ha cobrado ningún importe.",
+error_crawling_unreachable:
+"Artículo <strong>{0}</strong> no disponible en <strong>{1}</strong>. Asegúrese de que la URL del producto está disponible.",
+error_crawling_product_not_found:
+"Artículo <strong>{0}</strong> no disponible en <strong>{1}</strong>.",
+error_crawling_price_not_found:
+"El artículo <strong>{0}</strong> no tiene ningún precio especificado en <strong>{1}</strong>, especificarlo con data-item-price.",
+error_crawling_price_doesnot_match:
+"El precio del artículo <strong>{0}</strong> en <strong>{3}</strong> es <strong>{2}</strong> pero debería ser <strong>{1}</strong>.",
+error_crawlingfailed_title_test:
+"Algo salió mal cuando validábamos su pedido, no se preocupe, no se ha realizado ningún cargo. Esta página esta en modo Test.",
+order_completedon:
+"Colocado en",
+payment_method_status:
+"Estado de la transacción",
+payment_method_status_approved:
+"Aprobado",
+order_reference_number:
+"Número de referencia",
+order_transaction_amount:
+"Transacción",
+order_invoice_number:
+"Número de factura",
+order_authorization_code:
+"Código de autorización",
+item_is_being_added:
+"Añadiendo el artículo al carrito...",
+order_completing_payment:
+"Ejecutando pedido...",
+calculating_shipping_fees:
+"Calculando tarifas de envío...",
+saving:
+"Guardando...",
+loading:
+"Cargando...",
+cart_plans_interval:
+"Intervalo",
+cart_plans_interval_count:
+"Contar",
+subscription_plan_interval:
+"Intervalo de plan",
+subscription_plan_interval_count:
+"Plan intervalo cuenta",
+plan_amount_per_day:
+"{0} / día",
+plan_amount_per_week:
+"{0} / semana",
+plan_amount_per_month:
+"{0} / mes",
+plan_amount_per_year:
+"{0} / año",
+plan_amount_per_day_plural:
+"{0} / {1} días",
+plan_amount_per_week_plural:
+"{0} / {1} sermanas",
+plan_amount_per_month_plural:
+"{0} / {1} meses",
+plan_amount_per_year_plural:
+"{0} / {1} años"
+});
